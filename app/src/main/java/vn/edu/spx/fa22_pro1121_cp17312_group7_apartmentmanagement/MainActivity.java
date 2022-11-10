14d4be8b669ada0ac2f4d209081072bac516690b
Rog Strix <linhvo.928095200@gmail.com>
2022-11-10 14:25:40 +0700

    initial commit

---
+++ app/src/main/java/vn/edu/spx/fa22_pro1121_cp17312_group7_apartmentmanagement/MainActivity.java
@@ -0,0 +1,14 @@
+package vn.edu.spx.fa22_pro1121_cp17312_group7_apartmentmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
